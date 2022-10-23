eed8e3ea6a242aa031a651e1a23f18c03b23d5b4
эрвин бекиров <tom.margan@mail.ru>
2022-10-23 17:02:48 +0300

    Создание страниц

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/Hamond', function(req, res, next) {
+  res.send("<h1>Страница Ричарда Хамонда</h1>")
+});
+router.get('/Clarcson', function(req, res, next) {
+  res.send("<h1>Страница Джереми Кларксона</h1>")
+});
+router.get('/Mey', function(req, res, next) {
+  res.send("<h1>Страница Джейм Мея</h1>")
+});
 
 module.exports = router;
