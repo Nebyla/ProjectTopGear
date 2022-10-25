ef04f21bf821a26379d685e50d70a16b8686223a
эрвин бекиров <tom.margan@mail.ru>
2022-10-25 16:05:57 +0300

    «Заполнены»

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/Hamond', function(req, res, next) {
-  res.send("<h1>Страница Ричарда Хамонда</h1>")
+  res.render('Hamond', {
+    title: "Ричард Хамонд",
+    picture: "images/Hamond.jpg",
+    desc: "Английский телеведущий, наиболее известен по телепередачам «Top Gear»,"
+});
 });
 router.get('/Clarcson', function(req, res, next) {
-  res.send("<h1>Страница Джереми Кларксона</h1>")
+  res.render('Clarcson', {
+    title: "Джереми Кларксон",
+    picture: "images/clarkson.jpg",
+    desc: "английский телеведущий, фермер и журналист, специализирующийся на автомобильной тематике. Широко известен как ведущий телевизионных шоу «Top Gear»"
+});
 });
 router.get('/Mey', function(req, res, next) {
-  res.send("<h1>Страница Джейм Мея</h1>")
+  res.render('Mey', {
+    title: "Джеймс Мей",
+    picture: "images/Mey.jpg",
+    desc: "английский журналист, известный как один из ведущих телепередач Top Gear"
 });
-
+});
 module.exports = router;
