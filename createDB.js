e17b22ff0c050d05bdfe9c6cadf36556a018b0cf
эрвин бекиров <tom.margan@mail.ru>
2022-10-29 14:44:53 +0300

    Проверка Базы Данных

--- createDB.js
+++ createDB.js
@@ -1,18 +1,20 @@
 var MongoClient = require('mongodb').MongoClient
+var data = require("./data.js").data
 
 const uri = "mongodb://localhost:27017/"
 const client = new MongoClient(uri)
 async function run() {
 try {
 await client.connect();
-var database = client.db("TopGear");
+var database = client.db("ProjectTopGear");
 database.dropDatabase()
-database = client.db("TopGear");
-const cats = database.collection("Gear");
-const result = await cats.insertOne({name:"Кларксон"});
-console.log(`${result} documents were inserted`);
+database = client.db("ProjectTopGear");
+const Gear = database.collection("Gear");
+const result = await Gear.insertMany(data);
+console.log(`${result.insertedCount} documents were inserted`);
 } finally {
 await client.close();
 }
 }
 run()
+
