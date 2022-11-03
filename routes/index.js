27da96d79f1671b044733609b648d28c4b97b078
Nebyla <95207952+Nebyla@users.noreply.github.com>
2022-11-03 12:46:28 +0300

    Логика пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Gear = require("../models/gear").Gear
+var User = require("./../models/user").User
 
 
 /* GET home page. */
@@ -19,12 +20,29 @@ router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   }); 
   
-  router.post('/logreg', function(req, res, next) {
+    router.post('/logreg', function(req, res, next) {
     var username = req.body.username
     var password = req.body.password
-
-
+    User.findOne({username:username},function(err,user){
+        if(err) return next(err)
+        if(user){
+            if(user.checkPassword(password)){
+                req.session.user = user._id
+                res.redirect('/')
+            } else {
+                      res.render('logreg', {title: 'Вход'})
+            }
+        } else {
+            var user = new User({username:username,password:password})
+            user.save(function(err,user){
+                if(err) return next(err)
+                req.session.user = user._id
+                res.redirect('/')
+            })        
+      }
+    })
 });
+
 
 
 /*router.get('/Hamond', function(req, res, next) {
