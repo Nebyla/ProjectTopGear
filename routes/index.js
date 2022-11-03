0a8e0aec6d912f252813f09252be08b4c4bb8823
Nebyla <95207952+Nebyla@users.noreply.github.com>
2022-11-03 10:12:32 +0300

    Команда записи в cookie

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ var Gear = require("../models/gear").Gear
 /* GET home page. */
 router.get('/', function(req, res, next) {
     Gear.find({},{_id:0,title:1,nick:1},function(err,menu){
-        res.render('index', {
-                                title: 'Express',
-                                menu: menu
-                            });
+      res.cookie('greeting','Hi!!!').render('index', {
+        title: 'Express',
+        menu: menu
+    });
     })
 
 });
