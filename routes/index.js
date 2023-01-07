2cae443adadb1895edd3e11cdd98289c9d02fba7
эрвин бекиров <tom.margan@mail.ru>
2023-01-07 18:12:06 +0300

    Создание таблицы User

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Top Gear', counter:req.session.counter  });
 });
 
-/* GET login/registration page. */
+
 router.get('/logreg', function(req, res, next) {
 res.render('logreg',{title: 'Вход', error: null});
 });
 
-/* POST login/registration page. */
+
+
 router.post('/logreg', function(req, res, next) {
   var username = req.body.username
   var password = req.body.password
   // console.log("hey")
   db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`, function(err,users){
         if(err) return next(err)
-        // console.log("hey1")
-        // console.log(users)
-        // console.log(err)
         if(users.length > 0) {
-          // console.log("hey3")
           var user = users[0];
           if (password == user.password){
             req.session.user = user.user_id
@@ -35,13 +32,16 @@ router.post('/logreg', function(req, res, next) {
           }
         } else {
           db.query(`INSERT INTO user (username, password) VALUES ('${username}', '${password}')`, function(err, user){
-            // console.log(user)
-            // console.log(err)
             if(err) return next(err)
             req.session.user = user.user_id
             res.redirect('/')
           })
         }
 })
+});
+router.post('/logout', function(req, res, next) {
+  req.session.destroy()
+  req.session.user = null
+  res.redirect('/')
 });
 module.exports = router;
